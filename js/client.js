7073a1c1629c13e41f029b2a5b940e4e10337b03
Pheonix0101 <purushottam5252@gmail.com>
2023-05-23 08:54:42 +0530

    first commit

---
+++ js/client.js
@@ -0,0 +1,50 @@
+const socket = io("http://localhost:9000");
+
+// import { io } from "socket.io-client";
+
+socket = io({
+  extraHeaders: {
+    "my-custom-username": "purushottam",
+    "my-custom-password": "1234",
+  },
+});
+
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("messageInp");
+const messageContainer = document.querySelector(".container");
+
+var ntune = new Audio("./assests/tune.mp3");
+
+const append = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
+  if (position == "left") {
+    ntune.play();
+  }
+};
+
+form.addEventListener("submit", (p) => {
+  p.preventDefault();
+  const message = messageInput.value;
+  append(`you: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "";
+});
+
+const name = prompt("Enter your name");
+
+socket.emit("new-user-joined", name);
+
+socket.on("userjoined", (name) => {
+  append(`${name}: joined the chat`, "right");
+});
+socket.on("receive", (data) => {
+  append(`${data.name}: ${data.message}`, "left");
+});
+
+socket.on("leave", (name) => {
+  append(`${name} left the chat`, "right");
+});
